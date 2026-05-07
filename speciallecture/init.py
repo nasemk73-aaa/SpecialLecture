fbac7452fc71f649255f0ef251c6bcad2a999976
senamukai <166784937+sena-mukai@users.noreply.github.com>
2026-05-07 18:17:10 +0900

    Create programme

--- speciallecture/init.py
+++ speciallecture/init.py
@@ -1,1 +1,12 @@
-def Calculate:
+import csv
+
+
+class CSVPrinter:
+    def init(self, file_name):
+        self.file_name = file_name
+
+    def read(self):
+        with open(self.file_name) as f:
+            reader = csv.reader(f)
+            lines = [row for row in reader]
+        return lines
