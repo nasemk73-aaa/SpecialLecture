be8f9a3023b0b5dededde7be01e0a3f38ddca99d
senamukai <166784937+sena-mukai@users.noreply.github.com>
2026-05-07 18:26:53 +0900

    Create programme

--- tests/init.py
+++ tests/init.py
@@ -1,1 +1,10 @@
-def Calculate:
+import unittest
+
+from speciallecture.CSVPrinter import CSVPrinter
+
+class TestCSVPrinter(unittest.TestCase):
+    def test_read(self):
+        printer = CSVPrinter("sample.csv")
+        line = printer.read()
+        print(line)
+        self.assertEqual(3, len(line))
