9c7218279a02e9115345f3924551e2be71febdea
senamukai <166784937+sena-mukai@users.noreply.github.com>
2026-05-27 11:35:58 +0900

    Modify CodeSmell file

--- tests/tests_CSVPrinter.py
+++ tests/tests_CSVPrinter.py
@@ -28,17 +28,4 @@ class TestCSVPrinter(unittest.TestCase):
         else:
             result = False
         #Assertion Obscurity
-        self.assertEqual(True, result)
-
-
-
-
-
-"""
-class TestCSVPrinter(unittest.TestCase):
-    def test_read(self):
-        printer = CSVPrinter("sample.csv")
-        line = printer.read()
-        print(line)
-        self.assertEqual(3, len(line))
-"""
+        self.assertTrue(result)
