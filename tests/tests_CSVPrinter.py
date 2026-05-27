046f9ef7afc8015a9df2a6809d79a6c1021d226b
senamukai <166784937+sena-mukai@users.noreply.github.com>
2026-05-27 11:26:08 +0900

    Modify to CodeSmell

--- tests/tests_CSVPrinter.py
+++ tests/tests_CSVPrinter.py
@@ -2,15 +2,43 @@ import unittest
 
 from speciallecture.CSVPrinter import CSVPrinter
 
-
-def setUpModule():
-    print("Running setUpModule")
-def tearDownModule():
-    print("Running tearDownModule")
 class TestCSVPrinter(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        print("Running setUpClass")
-    @classmethod
-    def tearDownClass(cls):
-        print("Running tearDownClass")
+    def test_read(self):
+        #Magic Number / Magic String (意味不明な定数)
+        file_name = "sample.csv"
+        #わざと不要な変数を増やす
+        expected_line_count = 3
+        printer_object = CSVPrinter(file_name)
+        #わざと同じ処理を何度も書く
+        line1 = printer_object.read()
+        line2 = printer_object.read()
+        line3 = printer_object.read()
+
+        print("読み込んだ結果:")
+        print(line1)
+        print(line2)
+        print(line3)
+
+        #わざと条件分岐を増やす
+        if line1 is not None:
+            if len(line1) == expected_line_count:
+                result = True
+            else:
+                result = False
+        else:
+            result = False
+        #Assertion Obscurity
+        self.assertEqual(True, result)
+
+
+
+
+
+"""
+class TestCSVPrinter(unittest.TestCase):
+    def test_read(self):
+        printer = CSVPrinter("sample.csv")
+        line = printer.read()
+        print(line)
+        self.assertEqual(3, len(line))
+"""
